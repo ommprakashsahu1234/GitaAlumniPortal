08b2134fe0b8e0a797c14b04fe5a80d1467f41ff
Omm Prakash Sahu <opsommprakash@gmail.com>
2025-03-02 17:41:19 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://autophile07:OpsOmm@123@cluster0.qnhj4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => {
+mongoose.connect("mongodb://127.0.0.1:27017/GitaAlumni").then(() => {
   console.log("Database Connected.");
 }).catch((err)=>{
     console.log('No Connection')
